78a499fb0397df4f42b8c87d966026b0b587e59a
jjunny <jjunyoung99@gmail.com>
2017-03-17 23:05:12 +0900

    The Answer of 4016

---
+++ codeup_4016.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int a, b, c, result;
+	int i = 1;
+	scanf("%d %d %d", &a, &b, &c);
+	for(i ; i <= c ; i++){
+		if(a % i == 0 || b % i == 0 || c % i == 0){
+			result = i;
+		}
+	}
+	printf("%d", result);
+	return 0;
+}
+
