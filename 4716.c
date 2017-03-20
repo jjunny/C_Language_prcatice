4806af9ccb2ebd88c44a6814443e6a48f516b9d3
jjunny <jjunyoung99@gmail.com>
2017-03-20 22:44:52 +0900

    The Answer of 4716

--- 4716.c
+++ 4716.c
@@ -1,23 +1,11 @@
-#include <stdio.h>
-
-int main(void){
-  int min = 101, result = 0, i;
-  int N[7];
-  for(i=0;i<7;i++){
-    scanf("%d\n", &N[i]);
-  }
-  for(i=0;i<7;i++){
-    if(N[i]%2==1){
-      result += N[i];
-      if(min >= N[i]){
-        min = N[i];
-      }
+#include<stdio.h>
+int r, s;
+int main() {
+    for (int i = 0, x, y; i < 10; i++) {
+        scanf("%d %d", &x, &y);
+        s += y - x;
+        r = r > s ? r : s;
     }
-  }
-  if(min == 101){
-    printf("-1");
-  }
-  else{
-    printf("%d\n%d", result, min);
-  }
+    printf("%d", r);
+    return 0;
 }
