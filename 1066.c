ccb456d030825e9a5b4cccc2b7d0fa07bd90b278
jjunny <jjunyoung99@gmail.com>
2017-04-07 22:28:08 +0900

    The Answer of 1066

---
+++ 1066.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+ int n, m, k;
+ scanf("%d %d %d", &n, &m, &k);
+ if(n%2==0) printf("%s\n", "even");
+ else printf("%s\n","odd");
+ if(m%2==0) printf("%s\n", "even");
+ else printf("%s\n","odd");
+ if(k%2==0) printf("%s\n", "even");
+ else printf("%s\n","odd");
+}
