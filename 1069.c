a81828143a99024b3fb1cb7ef18e1f57027e2d3f
jjunny <jjunyoung99@gmail.com>
2017-04-07 22:34:19 +0900

    The Answer of 1069

---
+++ 1069.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+ char n;
+ scanf("%c", &n);
+ switch(n)
+ {
+  case 'A':
+  printf("best!!!");
+  break;
+  case 'B':
+  printf("good!!");
+  break;
+  case 'C':
+  printf("run!");
+  break;
+  case 'D':
+  printf("slowly~");
+  break;
+  default:
+  printf("what?");
+  break;
+ }
+}
