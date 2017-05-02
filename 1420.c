82865d10f8c364a4dc8af3cb2a670584f39e62eb
jjunny <jjunyoung99@gmail.com>
2017-05-02 22:58:26 +0900

    yet not solved

---
+++ 1420.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+  int point[50], i, n, j, tmp;
+  char name[100];
+  scanf("%d", &n);
+  for(i=0;i<n;i++){
+    scanf("%s %d", &name[i], &point[i]);
+  }
+  
+}
