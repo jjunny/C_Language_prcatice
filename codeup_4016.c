8718f5d7dcaed28b128f56077192a62af2fccb82
jjunny <jjunyoung99@gmail.com>
2017-05-03 23:02:13 +0900

    The Answer of 4016

--- codeup_4016.c
+++ codeup_4016.c
@@ -5,11 +5,10 @@ int main(void){
 	int i = 1;
 	scanf("%d %d %d", &a, &b, &c);
 	for(i ; i <= c ; i++){
-		if(a % i == 0 || b % i == 0 || c % i == 0){
+		if(a % i == 0 && b % i == 0 && c % i == 0){
 			result = i;
 		}
 	}
 	printf("%d", result);
 	return 0;
 }
-
