04d606198ee810fdb478d978efdc462e6977be5b
jjunny <jjunyoung99@gmail.com>
2017-05-13 21:55:08 +0900

    The Answer of 10817.c

---
+++ BAEKJOON/10817.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if(a>b&&b>c) printf("%d", b);
+  else if(c>b%%b>a) printf("%d", b);
+  else if(b>a&&a>c) printf("%d", a);
+  else if(c>a&&a>b) printf("%d", a);
+  else if(a>c&&c>b) printf("%d", c);
+  else if(b>c&&c>a) printf("%d", c);
+  else if(a==b||b==c) printf("%d", b);
+  else if(b==a||a==c) printf("%d", a);
+  else if(a==c||c==b) printf("%d", c);
+}
