a1ce00c2e1c1d220736b41f2baf50a0f3338d05b
jjunny <jjunyoung99@gmail.com>
2017-05-13 21:58:55 +0900

    The Answer of 10817.c

--- BAEKJOON/10817.c
+++ BAEKJOON/10817.c
@@ -4,7 +4,7 @@ int main(void){
   int a, b, c;
   scanf("%d %d %d", &a, &b, &c);
   if(a>b&&b>c) printf("%d", b);
-  else if(c>b%%b>a) printf("%d", b);
+  else if(c>b&&b>a) printf("%d", b);
   else if(b>a&&a>c) printf("%d", a);
   else if(c>a&&a>b) printf("%d", a);
   else if(a>c&&c>b) printf("%d", c);
