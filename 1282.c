67bcd44bc5dabaf8162dfca10cfb4579e5ad642a
jjunny <jjunyoung99@gmail.com>
2017-05-20 22:38:39 +0900

    The Answer of 1282.c

---
+++ 1282.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  double n;
+  int i = 1, k, t, input;
+  scanf("%lf", &sqrt(n));
+  input = (int)n;
+  while(i<=n){
+      i++;
+  }
+  k = input - i*i;
+  t = i;
+  printf("%d %d", k, t);
+}
