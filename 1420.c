c49f0d76bb0bca41f59d862a2805060f1a9d7ef7
jjunny <jjunyoung99@gmail.com>
2017-06-03 22:45:44 +0900

    N Y S

--- 1420.c
+++ 1420.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
+void swap(char **p);
 int main(void){
   int i, n, j, point[100], tmp;
-  char *name[101], temp;
+  char *name[101];
   scanf("%d", &n);
   for(i=0;i<n;i++){
     scanf("%s %d", &name[i], &point[i]);
   }
-  for(i=0;i<n;i++){
-    for(j=0;j<n-1;j++){
-      if(point[j]>point[j+1]){
+  for(i=0;i<n-1;i++){
+    for(j=i+1;j<n;j++){
+      if(point[j]<point[j+1]){
         tmp = point[j];
         point[j] = point[j+1];
         point[j+1] = tmp;
-        temp = *name[j];
-        *name[j] = *name[j+1];
-        name[j+1] = temp;
+        swap(&name[i]);
       }
     }
   }
-  printf("%s", name[0]);
+  printf("%s", name[n-3]);
+}
+void swap(char **p)
+{
+  int i, j;
+  char *temp = p[j];
+  p[j] = p[i];
+  p[i] = temp;
 }
