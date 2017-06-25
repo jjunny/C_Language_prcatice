964b66b392e70c7f908d64907f3de8eab17b99c4
jjunny <jjunyoung99@gmail.com>
2017-06-25 22:20:13 +0900

    3123123

--- test.c
+++ test.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
 int main(void){
-  int a, b, c;
-  char a, b, c;
-  long a, b, c;
-  int a[100], b[100], c[100];
-  char a[101], b[101], c[101];
-  unsigned int a, b, c;
-  long long a, b, c;
-  unsigned char a[101], b[101], c[101];
-  long double a, b, c;
-  double a, b, c;
-  long long int a, b, c;
+  int a;
 }
