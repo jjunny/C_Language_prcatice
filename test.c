c039aa5f683ef2b8ab0114f3f780fc8a9ac76720
jjunny <jjunyoung99@gmail.com>
2017-08-02 14:48:19 +0900

    124123124

--- test.c
+++ test.c
@@ -8,14 +8,14 @@ int main(void){
     }
   }
   for(i=0;i<10;j++){
-    for(j=10;j>0;j++){
+    for(j=10;j>0;j--){
       printf("%d x %d = %d", i, j, i*j);
     }
   }
-  for(i=10;i>0;i++){
+  for(i=10;i>0;i--){
     for(j=0;j<10;j++){
       printf("%d x %d = %d", i, j, i*j);
     }
   }
-  for()
+  for(i=10;i>0;i--)
 }
