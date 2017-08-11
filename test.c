fd69c29f7f14f22d3387f9115f8b3f6c0359b92b
jjunny <jjunyoung99@gmail.com>
2017-08-11 19:58:18 +0900

    1212312414

--- test.c
+++ test.c
@@ -1,26 +1,5 @@
 #include <stdio.h>
 
 int main(void){
-  int a, b, c, i, j;
-  for(i=0;i<10;i++){
-    for(j=0;j<10;j++){
-      printf("%d x %d = %d\n", i, j, i*j);
-    }
-  }
-  for(i=0;i<10;j++){
-    for(j=10;j>0;j--){
-      printf("%d x %d = %d", i, j, i*j);
-    }
-  }
-  for(i=10;i>0;i--){
-    for(j=0;j<10;j++){
-      printf("%d x %d = %d", i, j, i*j);
-    }
-  }
-  for(i=10;i>0;i--){
-    for(j=10;j>0;j--){
-      printf("%d x %d = %d", i, j, i*j);
-    }
-  }
-  return 0;
+
 }
