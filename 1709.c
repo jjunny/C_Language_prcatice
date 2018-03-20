88724a34c54786f6faad70ee2d9d68e5f42be541
jjunny <jjunyoung99@gmail.com>
2018-03-20 13:53:52 +0900

    The Answer of 1709

---
+++ 1709.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void QuickSort(int arr[], int left, int right){
+  int L = left, R = right;
+  int temp;
+  int pivot = arr[(left + right) / 2];
+  while(L <= R){
+    while(arr[L] < pivot)
+      L++;
+    while(arr[R] > pivot)
+      R--;
+    if(L <= R){
+      if(L != R){
+        temp = arr[L];
+        arr[L] = arr[R];
+        arr[R] = temp;
+      }
+      L++; R--;
+  }
+  if(left<R)
+    QuickSort(arr, left, R);
+  if(L<right)
+    QuickSort(arr, L, right);
+  }
+}
+
+int main(void) {
+  int N, data[100];
+  scanf("%d\n", &N);
+  for(int i=0;i<N;i++){
+    scanf("%d ", &data[i]);
+  }
+  QuickSort(data, 0, N-1);
+  for(int i=N-1;i>=0;i--){
+    printf("%d ", data[i]);//내림차순출력
+  }
+  return 0;
+}
